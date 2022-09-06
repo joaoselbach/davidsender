5acb1ef21c593cb080bc4d2a6f79d64fd9ca805a
Joao Selbach <joao.selbach@flieger.software>
2022-09-05 22:34:17 -0300

    feature: facebook pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,23 @@ import '../../node_modules/swiper/swiper-bundle.min.css'
 import '../../node_modules/swiper/swiper.min.css'
 import 'react-awesome-lightbox/build/style.css'
 import Script from 'next/script'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const router = useRouter()
+
+useEffect(() => {
+  import('react-facebook-pixel')
+    .then(x => x.default)
+    .then(ReactPixel => {
+      ReactPixel.init('1017784671692933')
+      ReactPixel.pageView()
+
+      router.events.on('routeChangeComplete', () => {
+        ReactPixel.pageView()
+      })
+    })
+}, [router.events])
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -22,20 +39,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   gtag('js', new Date());
   
   gtag('config', 'AW-444071114');`}</Script>
-      <Script strategy="lazyOnload">{`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1017784671692933');
-fbq('track', 'PageView');
-fbq('trackCustom', 'ViuPGVendas');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1017784671692933&ev=PageView&noscript=1"`}</Script>
+
       <ChakraProvider theme={theme}>
         <SimpleReactLightbox>
           <Component {...pageProps} />
